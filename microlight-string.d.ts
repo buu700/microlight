b861b6e94f43837145fcecea16a0829a30e119cb
Ryan Lester <hacker@linux.com>
2017-04-26 22:56:07 -0400

    type definition fix

--- microlight-string.d.ts
+++ microlight-string.d.ts
@@ -1,6 +1,8 @@
-export interface IMicrolight {
-	/** Processes code in text into syntax-highlighted html. */
-	process (text: string, color: string) : string;
-};
+declare module 'microlight-string' {
+	interface IMicrolight {
+		/** Processes code in text into syntax-highlighted html. */
+		process (text: string, color: string) : string;
+	}
 
-export const microlight: IMicrolight;
+	const microlight: IMicrolight;
+}
